b6652a354fe5fb53504b5bd4887564267836264d
Jayshree25h <jayshreeminuhande@gmail.com>
2026-09-18 20:31:52 +0530

    Day 5

---
+++ Day 5/largestno.py
@@ -0,0 +1,7 @@
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+if a > b:
+    print("A is a greatest number")
+else:
+    print("B is a greatest number")
+   
---
+++ largestofthree.py
@@ -0,0 +1,9 @@
+a =int (input("Enter fisrt number: "))
+b =int (input("Enter second number: "))
+c =int (input("Enter third number: "))
+if a > b and a > c:
+    print("A is the greatest number")
+elif b > a and b > c:
+    print ("B is the greatest number")
+else:
+    print("C is the greatest number")
